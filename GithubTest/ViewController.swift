67340978b55e65ff88725288afc2348bda2afe24
Peter Mårtensson <amatex100@gmail.com>
2017-01-29 22:15:49 +0100

    Initial Commit

---
+++ GithubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubTest
+//
+//  Created by Peter Mårtensson on 2017-01-29.
+//  Copyright © 2017 Peter Mårtensson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
